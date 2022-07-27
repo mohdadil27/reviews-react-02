cb12eb9a1c8acc5489fbd0042ee78ca5ce3f2bbb
mohdadil27 <mohdadil2721@gmail.com>
2022-07-27 14:38:44 +0530

    Updates

---
+++ precache-manifest.6373ea74158dabff015cffa6b9c10fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ebdc245d89c39316c99bd14d0c3841",
+    "url": "/reviews-react-02/index.html"
+  },
+  {
+    "revision": "c77341304c161a6c40a7",
+    "url": "/reviews-react-02/static/css/main.145cb26c.chunk.css"
+  },
+  {
+    "revision": "921f077836f6d456bf8e",
+    "url": "/reviews-react-02/static/js/2.ed824c14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviews-react-02/static/js/2.ed824c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77341304c161a6c40a7",
+    "url": "/reviews-react-02/static/js/main.9609f4ff.chunk.js"
+  },
+  {
+    "revision": "2cce4a68a8a00136f239",
+    "url": "/reviews-react-02/static/js/runtime-main.27840d83.js"
+  }
+]);
